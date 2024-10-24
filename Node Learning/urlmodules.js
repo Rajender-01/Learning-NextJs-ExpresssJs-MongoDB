10712dbc768d3940b6c818d8ec37e2c67a1d50ed
Rajender kumar <kumar.rajender@rwit.io>
2024-10-24 17:19:42 +0530

    Learn the url modules

---
+++ Node Learning/urlmodules.js
@@ -0,0 +1,13 @@
+import { URL } from 'url';
+const myURL = new URL("https://www.example.com:8080/p/a/t/h?query=string#hash");
+console.log('myURL.hash ==>', myURL.hash);
+console.log('myURL.host ==>', myURL.host);
+console.log('myURL.hostname ==>', myURL.hostname);
+console.log('myURL.href ==>', myURL.href);
+console.log('myURL.pathname ==>', myURL.pathname);
+console.log('myURL.port ==>', myURL.port);
+console.log('myURL.protocol ==>', myURL.protocol);
+console.log('myURL.search ==>', myURL.search);
+console.log('myURL.searchParams ==>', myURL.searchParams);
+console.log('myURL.toString() ==>', myURL.toString());
+console.log('myURL.toJSON ==>', myURL.toJSON());
