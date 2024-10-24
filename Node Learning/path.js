3c1af5df5d9b2f23202bb6f6a56cf6b0cdb131ee
Rajender kumar <kumar.rajender@rwit.io>
2024-10-24 12:26:49 +0530

    Path module in node js

---
+++ Node Learning/path.js
@@ -0,0 +1,10 @@
+// const path = require('path');
+// console.log('__filename :>> ', __filename);
+// console.log('__dirname :>> ', __dirname);
+// console.log(path.basename(__ilename));
+// console.log(path.basename(__filename, '.js'));
+// console.log(path.dirname(__filename));
+// console.log(path.extname(__filename));
+// console.log(path.join('/search', 'label', 'course/python','../..'))
+// console.log(path.normalize('C:\\Program\\\\tests'))
+// console.log(path.parse(__filename));
